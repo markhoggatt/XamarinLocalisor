af19b0e77849186806c06470b26a91acb577abf6
Mark Hoggatt <mark.hoggatt@paxton.co.uk>
2018-03-01 14:06:52 +0000

    Initial Commit

---
+++ XamarinLocalisor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XamarinLocalisor
+//
+//  Created by Mark Hoggatt on 01/03/2018.
+//  Copyright © 2018 Mark Hoggatt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
